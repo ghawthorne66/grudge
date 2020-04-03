b14cf55417fc213c89ff029156e0e18bdffcda3d
Greg Hawthorne <ghawthorne@me.com>
2020-04-02 20:01:27 -0700

    update lodash

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
+import Amplify from 'aws-amplify';
+import Configuration from './aws-exports'
+
+Amplify.configure(Configuration);
 
 ReactDOM.render(<Application />, document.getElementById('root'));
